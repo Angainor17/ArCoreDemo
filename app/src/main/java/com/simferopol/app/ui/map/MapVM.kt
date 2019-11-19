83b8a674035e6c34b608887eb6c55b3b2a3961d3
Anavol <Anavol777@gmail.com>
2019-11-19 19:18:54 +0300

    mapScreen buttons

--- app/src/main/java/com/simferopol/app/ui/map/MapVM.kt
+++ app/src/main/java/com/simferopol/app/ui/map/MapVM.kt
@@ -1,5 +1,6 @@
 package com.simferopol.app.ui.map
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -7,16 +8,64 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.simferopol.api.apiManager.ApiManager
+import com.simferopol.api.models.GeoObject
+import com.simferopol.app.App
+import com.simferopol.app.ui.routes.routeGeoObjects.vm.GeoObjectVm
 import com.simferopol.app.utils.models.ViewState
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.kodein.di.generic.instance
 
 val simfer = LatLng(44.949684, 34.102521)
 val simferBounds = LatLngBounds(LatLng(44.888679, 34.010726), LatLng(45.009175, 34.191087))
 
 class MapVM : ViewModel(), OnMapReadyCallback {
 
+    var currentZoom = 14f
+
+
+    fun onSelectRouteClick() {
+        Log.e("selectRoute", "click")// todo navigate to select route
+    }
+
+    fun onArClick() {
+        Log.e("ar", "click")// todo navigate to AR Screen
+    }
+
+    fun onLocateClick() {
+        Log.e("locate", "click")// todo locate
+    }
+
+    fun onZoomInClick() {
+        Log.e("zoomIn", "click")// todo zoomIn
+        currentZoom += 1f
+        map?.animateCamera(CameraUpdateFactory.zoomTo(currentZoom), 400, null)
+    }
+
+    fun onZoomOutClick() {
+        Log.e("zoomOut", "click")// todo zoomOut
+        currentZoom -= 1f
+        map?.animateCamera(CameraUpdateFactory.zoomTo(currentZoom), 400, null)
+    }
+
     var map: GoogleMap? = null
 
     val viewState = MutableLiveData(ViewState.LOADING)
+
+    private val routeManager by App.kodein.instance<ApiManager>()
+
+    val list = MutableLiveData(ArrayList<GeoObject>())
+
+    init {
+        GlobalScope.launch {
+            val result = routeManager.getGeoObjects(null)
+            if (result.success) {
+                list.postValue(ArrayList(result.data?.map { it } ?: ArrayList()))
+            }
+        }
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
@@ -25,6 +74,10 @@ class MapVM : ViewModel(), OnMapReadyCallback {
         initGoogleMapAttrs()
 
         initMapPosition()
+
+//        list.value?.forEach { map!!
+//            .addMarker(MarkerOptions())
+//            .position(LatLng(it.lat, it.lon)) }
     }
 
     private fun initGoogleMapAttrs() {
@@ -47,6 +100,6 @@ class MapVM : ViewModel(), OnMapReadyCallback {
     private fun initMapPosition() {
         map?.setLatLngBoundsForCameraTarget(simferBounds)
         map?.moveCamera(CameraUpdateFactory.newLatLng(simfer))
-        map?.animateCamera(CameraUpdateFactory.zoomTo(14f), 400, null)
+        map?.animateCamera(CameraUpdateFactory.zoomTo(currentZoom), 400, null)
     }
 }
