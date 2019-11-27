855833e5d80b25ead3693b27d4acd0e3f8efa70f
angai <angainor17@gmail.com>
2019-11-28 00:15:06 +0300

    ar location

---
+++ app/src/main/java/com/simferopol/app/ui/arLocation/model/Venue.kt
@@ -0,0 +1,14 @@
+package com.simferopol.app.ui.arLocation.model
+
+data class Venue(
+
+    val name: String,
+
+    val address: String,
+
+    val lat: Double,
+
+    val long: Double,
+
+    val iconURL: String
+)
